02cff1cc3ecb82adc15b9fcd7b88994af085cb2f
marcos-cardozo <marcosnazarenocardozo@gmail.com>
2025-02-20 17:10:45 -0300

    NavBar-And-Footer/marcos-navbar-and-footer/components/agregue el logo a la pagina

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -2,7 +2,7 @@ import GymFlowBanner from "@/components/GymFlowBanner/GymFlowBanner";
 
 export function Home() {
   return (
-    <div className="bg-[#1E1E1E]">
+    <div >
       <GymFlowBanner />
       <h1 className="text-white">GymFlow</h1>
     </div>
