6fd1762deb7bfb6c6b925a63157bd47148f96537
marcos-cardozo <marcosnazarenocardozo@gmail.com>
2025-02-21 16:57:41 -0300

    cambios en el navbar para que aparezca mi cuenta

--- front/src/data/NavItems.ts
+++ front/src/data/NavItems.ts
@@ -5,10 +5,11 @@ export interface NavItem {
 }
 
 const NavItems: NavItem[] = [
-  { texto: 'Inicio', link: '/', id: 1 },
-  { texto: 'Planes', link: '/Plans', id: 2 },
-  { texto: 'Sobre Nosotros', link: '/About', id: 3 },
-  { texto: 'Iniciar Sesion', link: '/Login', id: 4 },
+  { texto: "Inicio", link: "/", id: 1 },
+  { texto: "Planes", link: "/Plans", id: 2 },
+  { texto: "Sobre Nosotros", link: "/About", id: 3 },
+  { texto: "Iniciar Sesion", link: "/Login", id: 4 },
+  { texto: "Mi cuenta", link: "/MyAccount", id: 5 },
 ];
 
 export default NavItems;
