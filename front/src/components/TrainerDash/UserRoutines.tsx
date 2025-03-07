e610bcd8c3b000de822fe58f031bc086662a307a
LDFuentesAscanio <ludafuasca@gmail.com>
2025-03-07 16:30:54 -0300

    Dashboard del usuario

--- front/src/components/TrainerDash/UserRoutines.tsx
+++ front/src/components/TrainerDash/UserRoutines.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import RoutineView from '../RoutineView/RoutineView';
+import RoutineView from '../UserDashboard/RoutineView/RoutineView';
 
 const UserRoutines = () => {
-    return (
-        <div className="mb-6">
-          <h2 className="text-xl font-odor text-primary mb-4">Rutinas de Usuarios</h2>
-          <RoutineView />
-        </div>
-      );
+  return (
+    <div className="mb-6">
+      <h2 className="text-xl font-odor text-primary mb-4">
+        Rutinas de Usuarios
+      </h2>
+      <RoutineView />
+    </div>
+  );
 };
 
 export default UserRoutines;
