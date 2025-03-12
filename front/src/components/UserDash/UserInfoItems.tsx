8b75edb4edef7688dc87dfee4a574114c344249f
LDFuentesAscanio <ludafuasca@gmail.com>
2025-03-12 16:29:59 -0300

    Dashboard de Usuario

---
+++ front/src/components/UserDash/UserInfoItems.tsx
@@ -0,0 +1,17 @@
+interface UserInfoItemProps {
+  label: string;
+  value: string;
+}
+
+const UserInfoItem: React.FC<UserInfoItemProps> = ({ label, value }) => (
+  <div className="w-full">
+    <span className="text-sm sm:text-base">{label}:</span>
+    <div className="border-2 rounded-[10px] border-tertiary w-full h-auto min-h-10 text-xs sm:text-sm flex justify-center items-center px-3 py-2 break-words">
+      <span className="text-center font-holtwood leading-relaxed tracking-wide">
+        {value}
+      </span>
+    </div>
+  </div>
+);
+
+export default UserInfoItem;
--- front/src/data/MyAccountItems.ts
+++ front/src/data/MyAccountItems.ts
@@ -1,23 +1,27 @@
 export interface MyAccountItem {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
 export const MyAccountItems: MyAccountItem[] = [
-    {
-        id: 1,
-        name: "Nombre",
-    },
-    {
-        id: 2,
-        name: "Email"
-    },
-    {
-        id: 3,
-        name: "Telefono"
-    },
-    {
-        id: 4,
-        name: "Dirección"
-    }
-]
+  {
+    id: 1,
+    name: 'Nombre',
+  },
+  {
+    id: 2,
+    name: 'Email',
+  },
+  {
+    id: 3,
+    name: 'Telefono',
+  },
+  {
+    id: 4,
+    name: 'Dirección',
+  },
+  {
+    id: 5,
+    name: 'Membresía',
+  },
+];
