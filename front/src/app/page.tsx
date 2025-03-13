69c2ef45a8b6743dd4309ae5ea270f462b05d513
terrydiaz <89354469+terrydiaz@users.noreply.github.com>
2025-03-13 13:13:29 -0300

    geolocalizacion

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,6 +1,8 @@
 import GymFlowBanner from '@/components/Landing/GymFlowBanner/GymFlowBanner';
 import WhyChooseUs from '@/components/Landing/WhyChooseUs/WhyChooseUs';
 import SelectYourActivitie from '@/components/Landing/SelectYourActivity/SelectYourActivity';
+import MapComponent from '@/components/GoogleMaps/Maps';
+import ContactForm from '@/components/FormContactanos/FormCotactanos';
 
 export default function Page() {
   return (
@@ -8,6 +10,17 @@ export default function Page() {
       <GymFlowBanner />
       <WhyChooseUs />
       <SelectYourActivitie />
+      <div className="container mx-auto px-4 py-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-center">
+          <div className="w-full">
+          <ContactForm />
+           
+          </div>
+          <div className="w-full h-[400px]">
+          <MapComponent />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
